75d7efd4a9329e3bf5a5a052f9c1c0ff1e27c68d
sydney miranda <sydmiranda@icloud.com>
2021-01-04 23:19:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |greeting|
-    print "Hello #{greeting}!"
+    puts "Hello #{greeting}!"
 end
 
 def list_dwarves(array)
