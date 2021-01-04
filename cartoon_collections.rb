391578d993ab78f81d6a24928153c86d293be408
sydney miranda <sydmiranda@icloud.com>
2021-01-04 23:31:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,5 +10,5 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each.with_index(1) do |element, index|
-    puts "#{number}. #{element}" }
+    puts "#{index}. #{element}" }
 end
